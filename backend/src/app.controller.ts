2e478db8a6725bcabc67dff4781c16ba3bff7598
Лопатюков Артур <lopatyukovwork@gmail.com>
2018-09-19 10:01:34 +0300

    Added favicon Fixed error

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,15 +1,16 @@
-import { Get, Controller, Render, Res } from '@nestjs/common';
+import { Get, Controller, Render, Res, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { Redirect } from "@nestjs/common/decorators/http/redirect.decorator";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
-  @Render('index')
-  root(@Res() res: Response): any {
-    res.redirect('posts');
-    return { message: 'hello' };
+  @Redirect('posts')
+  root(): any {
+
   }
 }
